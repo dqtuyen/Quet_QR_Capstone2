8692612c6f758b7387f1f0ab8525c2dbcac2bac5
Dương Quang Tuyến <kasewdqt@gmail.com>
2024-05-09 23:03:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quet_qr_capstone2/Login.java
@@ -0,0 +1,14 @@
+package com.example.quet_qr_capstone2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Quet_QR_Capstone2"
+include(":app")
+ 
